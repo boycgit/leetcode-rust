109428dabc2935b346cae9abda5951a356adde27
boyc <boychenney@gmail.com>
2023-07-24 15:07:15 +0800

    refactor(cases): 更新 接雨水_42 题解
    采用单调栈方式解题

--- src/接雨水_42.rs
+++ src/接雨水_42.rs
@@ -14,26 +14,45 @@ impl Solution {
         let mut result = 0;
         let len = height.len();
 
-        let mut maxLeft = vec![0; len];
-        let mut maxRight = vec![0; len];
+        // 单调栈
+        let mut stack = vec![];
 
-        maxLeft[0] = 0;
-        maxRight[len - 1] = 0;
-
-        // 动态规划更新这两个数组
-        // 递推公式： maxLeft[i] = max(height[i - 1], maxLeft[i-1])
-        // maxRight[i] = max(height[i+1], maxRight[i+1]);
-        for i in 1..len {
-            maxLeft[i] = max(height[i - 1], maxLeft[i-1]);
-
-            let right_idx = len - 1 - i;
-            maxRight[right_idx] = max(height[right_idx + 1], maxRight[right_idx + 1]);
-        }
-
-        // 然后再遍历整个数组计算出雨水总量
-        // 某一点的计算公式：trap[i] = min(maxLeft[i], maxRight[i]) - height[i];
+        // 比较当前元素跟栈内的元素
         for i in 0..len {
-            result += max(0, min(maxLeft[i], maxRight[i]) - height[i]);
+            let cur = height[i];
+
+            // 根据栈顶元素做判断
+            while let Some(top_idx) = stack.pop() {
+
+                let top_num = height[top_idx];
+
+                // 如果相等的话，只需要将当前 i 压入，去掉之前的元素
+                if top_num == cur {
+                    // 啥都不做，毕竟栈顶已经弹出了
+                    break;
+                } else if top_num > cur {
+                    // 如果栈顶大于当前元素，恢复栈顶
+                    stack.push(top_idx);
+                    break;
+                } else {
+                    // 弹出元素直到比当前元素小，栈顶形成凹槽
+                    // 检查是否还有左凹槽
+                    if let Some(top_left_idx) = stack.last() {
+                        // 水滴高度
+                        let h = max(min(cur, height[*top_left_idx as usize]) - height[top_idx], 0);
+
+                        // 水滴宽度
+                        let w = i - *top_left_idx - 1;
+
+                        // 更新结果
+                        result += h * w as i32;
+                    }
+                }
+
+            }
+
+            // 判断完后无论如何都要将当前元素压入栈的
+            stack.push(i);
         }
 
         result
